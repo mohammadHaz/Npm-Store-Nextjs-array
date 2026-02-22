b20718652023d1e11335e69f6f04c069d8da2f9c
mohammadHaz <160180737+mohammadHaz@users.noreply.github.com>
2026-02-22 09:12:01 +0200

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+   reactStrictMode: false,
+  //  output: 'export', // مهم لتحويل المشروع إلى static site
+
+};
+
+export default nextConfig;
